71c974c9671e929cf2cd8819c45fb0e56083507a
AnzhelikaLanina <anzhelika.sidovi4eva@yandex.ru>
2024-07-31 00:10:53 +0300

    исправлена ошибка адреса

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.deploy.project.nomorepartiesco.ru/');
+  const api = new Api('https://api.deploy.project.nomorepartiesco.ru');
 
   export default api;
